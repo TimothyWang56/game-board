d8624411f063320812c7f7667ddb31b28bd989c1
TimothyWang56 <timothywang56@gmail.com>
2020-10-24 15:41:49 -0400

    create dropdown component

---
+++ src/Components/Dropdown/Dropdown.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './Dropdown.scss';
+
+class Dropdown extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            open: false
+        }
+    }
+
+    toggleDropdown() {
+        this.setState({
+            open: !this.state.open
+        })
+    }
+
+    render() {
+        return (
+            <div className='dropdown-wrapper'>
+                <div className='dropdown-top' onClick={this.toggleDropdown.bind(this)}>
+                    {this.props.options.length === 0 ?
+                        this.props.defaultText :
+                        this.props.options[this.props.selected].name}
+                </div>
+                {this.state.open && (
+                    <div className='dropdown-options'>
+                        {this.props.options.map((option, i) => (
+                            <div className={'option ' + (i === this.props.selected ? 'active' : '')}
+                                 key={i}
+                                 onClick={() => {
+                                     this.props.handleOptionSelect(i);
+                                     this.toggleDropdown();
+                                 }}>
+                                {option.name}
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
+
+export default Dropdown
