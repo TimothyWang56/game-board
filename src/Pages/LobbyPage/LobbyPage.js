ff6f94e83072905ff69329860169144bfe0e3c33
TimothyWang56 <timothywang56@gmail.com>
2020-10-24 13:21:27 -0400

    read forum posts from redux store

--- src/Pages/LobbyPage/LobbyPage.js
+++ src/Pages/LobbyPage/LobbyPage.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import './LobbyPage.scss';
 import Button from '../../Components/Button/Button';
+import ForumPost from '../../Components/ForumPost/ForumPost';
+import { connect } from 'react-redux';
 
 class LobbyPage extends Component {
     handleGoToLeaguesClick() {
         console.log('Go to leagues clicked!');
+    }
+
+    handleForumPostClick(id) {
+        console.log('Forum post ' + id + ' was clicked!');
     }
 
     render() {
@@ -12,14 +18,27 @@ class LobbyPage extends Component {
             <div className='page'>
                 <div className='lobby-header'>
                     <div className='title'>Lobby Forum</div>
-                    <div className='go-to-leagues-button button-text'>
+                    <div className='go-to-leagues-button small-text'>
                         <Button buttonText='Go To Your Leagues >' handleOnClick={this.handleGoToLeaguesClick.bind(this)}/>
                     </div>
                 </div>
                 <hr/>
+                {this.props.posts.map(post => {
+                    return (
+                        <div className='lobby-forum-post' key={post.id}>
+                            <ForumPost post={post} handleOnClick={this.handleForumPostClick}/>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
 }
 
-export default LobbyPage
+const mapStateToProps = (state) => {
+    return {
+        posts: state.forum
+    }
+}
+
+export default connect(mapStateToProps)(LobbyPage)
