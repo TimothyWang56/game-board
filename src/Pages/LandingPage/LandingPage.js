a4dc8b1e735645be7ad60b7fc426135785792aff
TimothyWang56 <timothywang56@gmail.com>
2020-10-23 20:55:30 -0400

    add landing page top banner

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './LandingPage.scss';
+import dice from '../../Assets/Images/dice.svg';
 
 class LandingPage extends Component {
     render() {
         return (
-            <div>
-                LandingPage
+            <div className='landing-page'>
+                <div className='landing-page-banner'>
+                    <img src={dice} alt='dice-svg'/>
+                    <div>
+                        <div className='game-board-title'>Game Board</div>
+                        <div className='game-board-description'>a board game dashboard for everyone!</div>
+                    </div>
+                    <img src={dice} alt='dice-svg'/>
+                </div>
             </div>
         )
     }
