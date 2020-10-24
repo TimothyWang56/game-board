96daa1b63ba9c2859b94a0c35441a9c64dc139a2
TimothyWang56 <timothywang56@gmail.com>
2020-10-24 15:41:59 -0400

    add dropdown to leagues page

--- src/Pages/LeaguesPage/LeaguesPage.js
+++ src/Pages/LeaguesPage/LeaguesPage.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Button from '../../Components/Button/Button';
+import Dropdown from '../../Components/Dropdown/Dropdown';
 import './LeaguesPage.scss';
+import { connect } from 'react-redux';
+import { selectLeague } from '../../actions/leagueActions';
+
 
 class LeaguesPage extends Component {
     handleGoToLobbyClick() {
@@ -18,6 +22,12 @@ class LeaguesPage extends Component {
                     <div className='go-to-lobby-button small-text'>
                         <Button buttonText='< Go Back to Lobby' handleOnClick={this.handleGoToLobbyClick.bind(this)}/>
                     </div>
+                    <div className='league-select-dropdown small-text'>
+                        <Dropdown
+                            selected={this.props.selectedLeague}
+                            options={this.props.leagues}
+                            handleOptionSelect={this.props.onDropdownClick}/>
+                    </div>
                     <div className='create-league-button small-text'>
                         <Button buttonText='+ Create League' handleOnClick={this.handleCreateLeagueClick}/>
                     </div>
@@ -28,4 +38,18 @@ class LeaguesPage extends Component {
     }
 }
 
-export default LeaguesPage
+const mapStateToProps = (state) => {
+    return {
+        ...state.leagues
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+      onDropdownClick: (index) => {
+        dispatch(selectLeague(index))
+      }
+    }
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(LeaguesPage)
