8f7b84dcedc9564ef0dea4a59b9f70ac2f7674e2
TimothyWang56 <timothywang56@gmail.com>
2020-10-30 16:55:49 -0400

    set up user actions

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,34 +1,55 @@
 export const LOG_IN_START = 'LOG_IN_START';
 export const LOG_IN_COMPLETE = 'LOG_IN_COMPLETE';
 export const LOG_IN_ERROR = 'LOG_IN_ERROR';
-export const LOG_OUT_START = 'LOG_OUT_START';
-export const LOG_OUT_COMPLETE = 'LOG_OUT_COMPLETE';
-export const LOG_OUT_ERROR = 'LOG_OUT_ERROR';
+export const LOG_OUT = 'LOG_OUT';
 export const REGISTER_START = 'REGISTER_START';
 export const REGISTER_COMPLETE = 'REGISTER_COMPLETE';
 export const REGISTER_ERROR = 'REGISTER_ERROR';
 
-// TODO: write action creators here
-
-export function loginSubmit(username, password) {
+export function loginStart(credentials) {
     return {
         type: LOG_IN_START,
-        username,
-        password
+        payload: credentials
     };
 }
 
-export function loginComplete(id, token) {
+export function loginComplete(user) {
     return {
         type: LOG_IN_COMPLETE,
-        id,
-        token
+        payload: user
     };
 }
 
 export function loginError(error) {
     return {
         type: LOG_IN_ERROR,
-        error,
+        payload: error,
     };
 }
+
+export function registerStart(credentials) {
+    return {
+        type: REGISTER_START,
+        payload: credentials,
+    };
+}
+
+export function registerComplete(user) {
+    return {
+        type: REGISTER_COMPLETE,
+        payload: user,
+    }
+};
+
+export function registerError(error) {
+    return {
+        type: REGISTER_ERROR,
+        payload: error,
+    }
+};
+
+export function logOut() {
+    return {
+        type: LOG_OUT,
+    }
+};
