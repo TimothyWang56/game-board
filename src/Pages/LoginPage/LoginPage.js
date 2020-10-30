de51934f16fba99b20b8568cf976fda927976f3a
TimothyWang56 <timothywang56@gmail.com>
2020-10-30 14:13:56 -0400

    create basic login page

---
+++ src/Pages/LoginPage/LoginPage.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import Button from '../../Components/Button/Button';
+import './LoginPage.scss'
+
+class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: '',
+            warning: false,
+        }
+    }
+
+    handleLogin() {
+        // make API call
+        const credentialsVerified = true;
+        if (credentialsVerified) {
+            this.props.history.push('/lobby');
+        } else {
+            this.setState({ warning: true });
+        }
+    }
+
+    handleGoToRegistration() {
+        console.log("go to registration!");
+    }
+
+    handleBackToLandingPage() {
+        this.props.history.push('/');
+    }
+
+    render() {
+        return (
+            <div className='page'>
+                <div className='login-wrapper text'>
+                    <div className='big-title'>
+                        Login
+                    </div>
+                    <label>
+                        Username:
+                        <input className='login-input' type='text' onChange={(event) => this.setState({ username: event.target.value })}/>
+                    </label>
+                    <label>
+                        Password:
+                        <input className='login-input' type='password' onChange={(event) => this.setState({ password: event.target.value })}/>
+                    </label>
+                    {this.state.warning &&
+                        <div className='warning'>
+                            username or password is incorrect
+                        </div>
+                    }
+                    <div className='login-page-buttons'>
+                        <div className='login-button'>
+                            <Button buttonText='Login' handleOnClick={this.handleLogin.bind(this)}/>
+                        </div>
+                        <div className='go-to-registration-button'>
+                            <Button buttonText='Register Now!' handleOnClick={this.handleGoToRegistration.bind(this)}/>
+                        </div>
+                    </div>
+                    <div className='back-to-landing-page-text' onClick={this.handleBackToLandingPage.bind(this)}>
+                        {"< go back to landing page"}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LoginPage
