dc15db78099ccd36426f3ae71cc8e2db07f49b45
TimothyWang56 <timothywang56@gmail.com>
2020-10-30 15:38:37 -0400

    change sign in to log in

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -1,13 +1,10 @@
 import {
-    SIGN_IN_PENDING,
-    SIGN_IN_COMPLETE,
-    SIGN_IN_ERROR,
+    LOG_IN_PENDING,
+    LOG_IN_COMPLETE,
+    LOG_IN_ERROR,
     LOG_OUT_PENDING,
     LOG_OUT_COMPLETE,
     LOG_OUT_ERROR,
-    CREATE_ACCOUNT_PENDING,
-    CREATE_ACCOUNT_COMPLETE,
-    CREATE_ACCOUNT_ERROR,
 } from '../actions/userActions';
 
 const initState = {
@@ -17,23 +14,17 @@ const initState = {
 // implement these later
 export default function user(state = initState, action) {
     switch(action.type) {
-        case SIGN_IN_PENDING:
+        case LOG_IN_PENDING:
             return state;
-        case SIGN_IN_COMPLETE:
+        case LOG_IN_COMPLETE:
             return state;
-        case SIGN_IN_ERROR:
+        case LOG_IN_ERROR:
             return state;
         case LOG_OUT_PENDING:
             return state;
         case LOG_OUT_COMPLETE:
             return state;
         case LOG_OUT_ERROR:
-            return state;
-        case CREATE_ACCOUNT_PENDING:
-            return state;
-        case CREATE_ACCOUNT_COMPLETE:
-            return state;
-        case CREATE_ACCOUNT_ERROR:
             return state;
         default:
             return state;
