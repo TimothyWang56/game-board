bbcd582f99edfe25c08adb7539b46ac2a6edf322
TimothyWang56 <timothywang56@gmail.com>
2020-10-30 15:56:00 -0400

    rename user actions

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,8 +1,34 @@
-export const LOG_IN_PENDING = 'LOG_IN_PENDING';
+export const LOG_IN_START = 'LOG_IN_START';
 export const LOG_IN_COMPLETE = 'LOG_IN_COMPLETE';
 export const LOG_IN_ERROR = 'LOG_IN_ERROR';
-export const LOG_OUT_PENDING = 'LOG_OUT_PENDING';
+export const LOG_OUT_START = 'LOG_OUT_START';
 export const LOG_OUT_COMPLETE = 'LOG_OUT_COMPLETE';
 export const LOG_OUT_ERROR = 'LOG_OUT_ERROR';
+export const REGISTER_START = 'REGISTER_START';
+export const REGISTER_COMPLETE = 'REGISTER_COMPLETE';
+export const REGISTER_ERROR = 'REGISTER_ERROR';
 
 // TODO: write action creators here
+
+export function loginSubmit(username, password) {
+    return {
+        type: LOG_IN_START,
+        username,
+        password
+    };
+}
+
+export function loginComplete(id, token) {
+    return {
+        type: LOG_IN_COMPLETE,
+        id,
+        token
+    };
+}
+
+export function loginError(error) {
+    return {
+        type: LOG_IN_ERROR,
+        error,
+    };
+}
--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -1,10 +1,13 @@
 import {
-    LOG_IN_PENDING,
+    LOG_IN_START,
     LOG_IN_COMPLETE,
     LOG_IN_ERROR,
-    LOG_OUT_PENDING,
+    LOG_OUT_START,
     LOG_OUT_COMPLETE,
     LOG_OUT_ERROR,
+    REGISTER_START,
+    REGISTER_COMPLETE,
+    REGISTER_ERROR,
 } from '../actions/userActions';
 
 const initState = {
@@ -14,17 +17,23 @@ const initState = {
 // implement these later
 export default function user(state = initState, action) {
     switch(action.type) {
-        case LOG_IN_PENDING:
+        case LOG_IN_START:
             return state;
         case LOG_IN_COMPLETE:
             return state;
         case LOG_IN_ERROR:
             return state;
-        case LOG_OUT_PENDING:
+        case LOG_OUT_START:
             return state;
         case LOG_OUT_COMPLETE:
             return state;
         case LOG_OUT_ERROR:
+            return state;
+        case REGISTER_START:
+            return state;
+        case REGISTER_COMPLETE:
+            return state;
+        case REGISTER_ERROR:
             return state;
         default:
             return state;
