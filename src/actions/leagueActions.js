573f7f376fc7644d728bb8fa63541867fc1773d7
TimothyWang56 <timothywang56@gmail.com>
2020-11-16 15:50:31 -0500

    add fetching actions for league data

--- src/actions/leagueActions.js
+++ src/actions/leagueActions.js
@@ -3,6 +3,30 @@ export const ADD_LEAGUE = 'ADD_LEAGUE';
 export const LEAVE_LEAGUE = 'LEAVE_LEAGUE';
 export const DELETE_LEAGUE = 'DELETE_LEAGUE';
 
+export const FETCH_LEAGUE_DATA_START = 'FETCH_LEAGUE_DATA_START'
+export const FETCH_LEAGUE_DATA_COMPLETE = 'FETCH_LEAGUE_DATA_COMPLETE'
+export const FETCH_LEAGUE_DATA_ERROR = 'FETCH_LEAGUE_DATA_ERROR'
+
+export function fetchLeagueDataStart() {
+    return {
+        type: FETCH_LEAGUE_DATA_START
+    }
+}
+
+export function fetchLeagueDataComplete(data) {
+    return {
+        type: FETCH_LEAGUE_DATA_COMPLETE,
+        payload: data
+    }
+}
+
+export function fetchLeagueDataError(error) {
+    return {
+        type: FETCH_LEAGUE_DATA_ERROR,
+        payload: error
+    }
+}
+
 export function selectLeague(index) {
     return {
         type: SELECT_LEAGUE,
