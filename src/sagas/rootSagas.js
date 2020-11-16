782095a12b5f4c45e9e1b138424a308f91ad1bd7
TimothyWang56 <timothywang56@gmail.com>
2020-11-16 15:54:09 -0500

    add leagueSagas to root

--- src/sagas/rootSagas.js
+++ src/sagas/rootSagas.js
@@ -1,7 +1,7 @@
 import { all, call } from 'redux-saga/effects';
-
+import { leagueSagas } from './leagueSagas';
 import { userSagas } from './userSagas';
 
 export default function* rootSaga() {
-    yield all([call(userSagas)]);
+    yield all([call(userSagas), call(leagueSagas)]);
 }
